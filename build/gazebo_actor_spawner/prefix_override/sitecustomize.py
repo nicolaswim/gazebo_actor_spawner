636b9396544babfd39707527ef0379f726fe7304
Wim <you@example.com>
2025-12-13 23:02:27 +0100

    Apply .gitignore and remove build artifacts from repo

--- build/gazebo_actor_spawner/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/wim/Documents/gazebo_actor_spawner/install/gazebo_actor_spawner'
--- build/gazebo_actor_spawner/src/share/gazebo_actor_spawner/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/wim/Documents/gazebo_actor_spawner/build/gazebo_actor_spawner/src"
